28be8c2afef83a5e7a3d06a2dfd926dbf34f8742
Víctor A. Montero <vamj809@users.noreply.github.com>
2021-10-07 21:18:40 -0400

    Add project files.

---
+++ MobileDev07.XamarinAdvanced/MobileDev07.XamarinAdvanced/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using MobileDev07.XamarinAdvanced.Views;
+
+namespace MobileDev07.XamarinAdvanced
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
